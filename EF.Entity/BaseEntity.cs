82c23eccd3e41e21e332d05e71d875ee5482387a
caofangsheng93 <fangsheng93@163.com>
2016-07-28 18:37:12 +0800

    0.0

--- EF.Entity/BaseEntity.cs
+++ EF.Entity/BaseEntity.cs
@@ -8,8 +8,22 @@ namespace EF.Core
 {
     public class BaseEntity
     {
+        /// <summary>
+        /// ID
+        /// </summary>
         public int ID { get; set; }
-
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime AddedDate { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime ModifiedDate { get; set; }
+        /// <summary>
+        /// IP地址
+        /// </summary>
+        public string IP { get; set; } 
        
 
     }
